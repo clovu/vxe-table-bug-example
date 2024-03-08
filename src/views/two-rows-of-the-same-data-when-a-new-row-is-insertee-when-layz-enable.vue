e2e2eb6f7b480cf3c811e3924b60739c5c6342b6
Clover <2621869236@qq.com>
2024-03-08 17:15:11 +0800

    懒加载 bug demo

--- src/views/two-rows-of-the-same-data-when-a-new-row-is-insertee-when-layz-enable.vue
+++ src/views/two-rows-of-the-same-data-when-a-new-row-is-insertee-when-layz-enable.vue
@@ -12,16 +12,63 @@ import { reactive, ref } from "vue"
 import { Button, VxeGrid, VxeGridInstance, VxeGridProps } from "vxe-table"
 
 const { gridRef, vxeGridProps, insertRow, getCurerntData } = useGrdid()
-
+interface RowVO {
+  id: number
+  parentId: number | null
+  name: string
+  type: string
+  size: number
+  date: string
+  hasChild?: boolean
+}
 function useGrdid() {
   const gridRef = ref<VxeGridInstance>()
-
+  const tableData = ref<RowVO[]>([
+    {
+      id: 10000,
+      parentId: null,
+      name: "test abc1",
+      type: "mp3",
+      size: 1024,
+      date: "2020-08-01",
+    },
+    {
+      id: 10050,
+      parentId: null,
+      name: "Test2",
+      type: "mp4",
+      size: 0,
+      date: "2021-04-01",
+      hasChild: true,
+    },
+    {
+      id: 23666,
+      parentId: null,
+      name: "Test23",
+      type: "mp4",
+      size: 0,
+      date: "2021-01-02",
+      hasChild: true,
+    },
+    {
+      id: 24555,
+      parentId: null,
+      name: "test abc9",
+      type: "avi",
+      size: 224,
+      date: "2020-10-01",
+    },
+  ])
   /*表格配置*/
-  const vxeGridProps = reactive<VxeGridProps>({
+  const vxeGridProps = reactive<VxeGridProps<RowVO>>({
     keepSource: true,
     height: 600,
     rowConfig: {
       isCurrent: true,
+    },
+    editConfig: {
+      showIcon: true,
+      showStatus: true,
     },
     treeConfig: {
       /*是否懒加载*/
@@ -33,91 +80,80 @@ function useGrdid() {
       /*对于同一级的节点，每次只能展开一个*/
       accordion: false,
       /*id*/
-      rowField: "markingId",
+      rowField: "id",
       /*父id*/
       parentField: "parentId",
       /*是否有子级*/
-      hasChild: "hasChild",
+      hasChildField: "hasChild",
       /*打开子级图标*/
       iconOpen: "vxe-icon-square-minus",
       /*关闭子级图标*/
       iconClose: "vxe-icon-square-plus",
       // 加载表格数据
-      loadMethod,
+      loadMethod({ row }) {
+        // 异步加载子节点
+        return fetchChildListApi(row)
+      },
     },
     columns: [
-      {
-        field: "id",
-        title: "id",
-        visible: false,
-      },
       {
         field: "name",
         title: "name",
         treeNode: true,
       },
-    ],
-    data: [
       {
-        parentId: "",
-        id: 0,
-        markingId: "0",
-        name: "订购",
-        isChild: true,
-        hasChild: false,
-        maType: "A",
-        type: 0,
-      },
-      {
-        hasChild: true,
-        markingId: "0-0",
-        isChild: true,
-        name: "0-0",
-        id: "0-0",
-        parentId: "0",
-        type: 1,
-        seqNo: null,
-        assortSeq: null,
+        field: "size",
+        title: "size",
       },
     ],
+    data: tableData as any,
   })
 
-  function loadMethod() {
-    return Promise.resolve([
-      {
-        hasChild: true,
-        markingId: "0-0-0",
-        isChild: true,
-        name: Date.now(),
-        id: 2022,
-        parentId: "0-0",
-        type: 2,
-        seqNo: 1,
-        assortSeq: null,
-      },
-    ])
+  // 模拟后台
+  const fetchChildListApi = (parentRow: RowVO) => {
+    return new Promise<RowVO[]>((resolve) => {
+      setTimeout(() => {
+        const childs = [
+          {
+            id: parentRow.id + 1000000,
+            parentId: parentRow.id,
+            name: parentRow.name + "Test111111111111",
+            type: "mp4",
+            size: 0,
+            date: "2021-10-03",
+            hasChild: true,
+          },
+          {
+            id: parentRow.id + 1500000,
+            parentId: parentRow.id,
+            name: parentRow.name + "Test222222222222",
+            type: "mp3",
+            size: 0,
+            date: "2021-07-09",
+            hasChild: false,
+          },
+        ]
+        resolve(childs)
+      }, 500)
+    })
   }
 
-  function insertRow() {
+  async function insertRow() {
     const grid = gridRef.value
     if (grid == void 0) return
 
     const curr = grid.getCurrentRecord()
+    await grid.setTreeExpand(curr, true)
 
-    grid.insertNextAt(
-      {
-        hasChild: false,
-        markingId: "2022-1-1",
-        isChild: false,
-        name: Date.now().toString(),
-        id: 2022,
-        parentId: "2022-1",
-        type: 3,
-        seqNo: 1,
-        assortSeq: 1,
-      },
-      curr
-    )
+    await grid.insert({
+      id: curr.id + 1500000,
+      parentId: curr.id,
+      name: curr.name + "Test222222222222",
+      type: "mp3",
+      size: 0,
+      date: "2021-07-09",
+      hasChild: false,
+    })
   }
 
   function getCurerntData() {
