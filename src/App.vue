53e6fc2dc33f9e30cbdaa3564aa0c6480838f6cf
Clover <2621869236@qq.com>
2024-07-23 09:48:00 +0800

    feat: github repositories url

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import { VxeTag, VxeTagPropTypes } from 'vxe-pc-ui';
 import { VxeGrid, VxeGridPropTypes } from 'vxe-table';
 
 const columns = reactive<VxeGridPropTypes.Columns>([
-  { title: 'Issue', width: 70, slots: { default: 'github-issue-url' }},
-  { title: 'Issue Status',width: 110, slots: { default: 'status' }},
-  { title: 'PR', width: 70, slots: { default: 'github-pr-url' }},
-  { title: 'Description', field: 'desc'},
+  { title: 'Issue', width: 70, slots: { default: 'github-issue-url' } },
+  { title: 'Issue Status', width: 110, slots: { default: 'status' } },
+  { title: 'PR', width: 70, slots: { default: 'github-pr-url' } },
+  { title: 'Description', field: 'desc' },
   { title: 'Action', slots: { default: 'action' } },
 ])
 
@@ -19,8 +19,8 @@ enum IssueStatus {
 }
 
 const dataSource = reactive([
-  {issue: 2320, status: IssueStatus.Fix, pr: 2428, desc: "TreeTable 懒加载数据首次新增行时出现重复数据 (4.7.56 修复)", },
-  {issue: 2479, status: IssueStatus.Close, pr: null, desc: "如果存在新增数据, getRowIndex 函数返回的序号错误 (需使用 getVTRowIndex 函数)" },
+  { issue: 2320, status: IssueStatus.Fix, pr: 2428, desc: "TreeTable 懒加载数据首次新增行时出现重复数据 (4.7.56 修复)", },
+  { issue: 2479, status: IssueStatus.Close, pr: null, desc: "如果存在新增数据, getRowIndex 函数返回的序号错误 (需使用 getVTRowIndex 函数)" },
 ])
 
 const router = useRouter()
@@ -41,10 +41,16 @@ const MatchStatusText = {
 </script>
 
 <template>
+  <div>
+    <a href="https://github.com/Clover-You/vxe-table-bug-example" target="_blank">
+      <img src="https://img.shields.io/github/stars/clover-you/vxe-table-bug-example.svg" />
+    </a>
+  </div>
+
   <router-view />
 
   <div style="margin-top: 20px; margin-bottom: 20px;">ISSUE 列表</div>
-  
+
   <VxeGrid :columns="columns" :data="dataSource">
     <template #status="{ row }">
       <VxeTag :status="MatchStatus[row.status as IssueStatus]">
@@ -63,9 +69,7 @@ const MatchStatusText = {
     </template>
 
     <template #github-pr-url="{ row }">
-      <a v-if="row.pr" :href="`https://github.com/x-extends/vxe-table/pull/${row.pr}`"
-        target="_blank"
-      >
+      <a v-if="row.pr" :href="`https://github.com/x-extends/vxe-table/pull/${row.pr}`" target="_blank">
         #{{ row.pr }}
       </a>
     </template>
