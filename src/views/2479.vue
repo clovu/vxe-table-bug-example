788b1280bdf1f712be5e3353d6e73861e6f1ab5c
Clover <2621869236@qq.com>
2024-07-23 09:38:34 +0800

    chore: 乌龙 BUG

--- src/views/2479.vue
+++ src/views/2479.vue
@@ -17,18 +17,37 @@ const dataSource = reactive([
 
 const gridRef = ref<VxeGridInstance>()
 
+const dialog = (index?: number) => {
+  VxeUI.modal.alert({
+    id: 'myUniqueAlert',
+    content: `当前选中行的行号是 ${index}`,
+    mask: false,
+    lockView: false
+  })
+}
+
 const currRowIndexDialog = () => {
   const grid = gridRef.value
   if (grid == void 0) return
 
   const curr = grid.getCurrentRecord()
+  dialog(grid.getRowIndex(curr))
+}
 
-  VxeUI.modal.alert({
-    id: 'myUniqueAlert',
-    content: `当前选中行的行号是 ${grid.getRowIndex(curr)}`,
-    mask: false,
-    lockView: false
-  })
+const getVMRowIndex = () => {
+  const grid = gridRef.value
+  if (grid == void 0) return
+
+  const curr = grid.getCurrentRecord()
+  dialog(grid.getVMRowIndex(curr))
+}
+
+const getVTRowIndex = () => {
+  const grid = gridRef.value
+  if (grid == void 0) return
+
+  const curr = grid.getCurrentRecord()
+  dialog(grid.getVTRowIndex(curr))
 }
 
 const insertRowHandler = () => {
@@ -43,14 +62,22 @@ const insertRowHandler = () => {
 
 <template>
   <VxeButton style="margin-bottom: 10px;" @click="currRowIndexDialog">
-    获取当前选中行 RowIndex
+    通过 getRowIndex 获取当前选中行
+  </VxeButton>
+
+  <VxeButton style="margin-bottom: 10px;" @click="getVMRowIndex">
+    通过 getVMRowIndex 获取当前选中行
+  </VxeButton>
+  
+  <VxeButton style="margin-bottom: 10px;" @click="getVTRowIndex">
+    通过 getVTRowIndex 获取当前选中行
   </VxeButton>
 
   <VxeButton style="margin-bottom: 10px;" @click="insertRowHandler">
     随便添加一行
   </VxeButton>
 
-  <VxeGrid ref="gridRef" :columns="columns" :data="dataSource" keep-source
-    :edit-config="{ showInsertStatus: true }" :row-config="{ isHover: true, isCurrent: true }">
+  <VxeGrid ref="gridRef" :columns="columns" :data="dataSource" keep-source :edit-config="{ showInsertStatus: true }"
+    :row-config="{ isHover: true, isCurrent: true }">
   </VxeGrid>
 </template>
